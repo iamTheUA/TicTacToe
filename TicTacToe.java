03c5f092b6d58a28b0463008fc8ea66a338e3552
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-02 19:22:11 +0530

    Selecting sign

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,10 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+
+	int uSign, cSign;
 
 	public static void main(String[] args) {
 		System.out.println("--Welcome to the Tic Tac Toe Game--");
@@ -14,5 +18,15 @@ public class TicTacToe {
 				board[i][j] = ' ';
 			}
 		}
+	}
+
+	public static void Select() {
+		System.out.println("Enter 1 for X or 2 for O :");
+		Scanner sc = new Scanner(System.in);
+		int uSign = sc.nextInt();
+		int cSign = (uSign == 1) ? 2 : 1;
+		System.out.print("You Selected" + ((uSign == 1) ? "X" : "O"));
+		System.out.print("Computer is :" + ((cSign == 1) ? "X" : "O"));
+		sc.close();
 	}
 }
