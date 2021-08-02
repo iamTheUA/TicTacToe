7a6c4c3b02487180b219ed2158086a9fbc55d570
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-02 19:06:12 +0530

    Welcoming

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,14 +5,6 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		System.out.println("--Welcome to the Tic Tac Toe Game--");
 		char board[][] = new char[3][3];
-		createBoard(board);
-	}
-
-	public static void createBoard(char board[][]) {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				board[i][j] = ' ';
-			}
-		}
+	
 	}
 }
